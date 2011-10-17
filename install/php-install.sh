fe3060605b47c8d0deaad9d4b088e8446944c5f6
agalkin <agalkin@ocg.ru>
2011-10-17 09:08:54 +0400

    php 5.3.8

--- install/php-install.sh
+++ install/php-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-PHP_VERSION="5.3.7"
+PHP_VERSION="5.3.8"
 INSTALL_BASE="/usr/local" # will make base/php-v.v.v.v folder and link base/php to it
 SOURCE_FOLDER="$HOME/sources"
 APACHE_FOLDER="/usr/local/apache2"
