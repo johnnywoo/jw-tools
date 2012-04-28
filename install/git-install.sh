25d6a0c28c2b0698db244ffc93330f959f59b968
johnnywoo <agalkin@agalkin.ru>
2012-04-28 08:07:38 +0400

    Updated git installation script

--- install/git-install.sh
+++ install/git-install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-GIT_VERSION="1.7.6"
+GIT_VERSION="1.7.10"
 INSTALL_BASE="/usr/local" # will make base/git-v.v.v.v folder and link base/git to it
 SOURCE_FOLDER="$HOME/sources"
 
 [ -e "$SOURCE_FOLDER" ] || mkdir "$SOURCE_FOLDER"
 cd "$SOURCE_FOLDER"
 
-wget --no-verbose http://kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.bz2
-tar xjf git-$GIT_VERSION.tar.bz2
+wget --no-verbose http://git-core.googlecode.com/files/git-$GIT_VERSION.tar.gz
+tar xzf git-$GIT_VERSION.tar.gz
 cd git-$GIT_VERSION
 ./configure --prefix=$INSTALL_BASE/git-$GIT_VERSION
 make
