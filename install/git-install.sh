1540d6eada3b67beef5c71b1646e426e800e34ee
agalkin <agalkin@ocg.ru>
2012-10-19 08:02:55 +0400

    Install version bump

--- install/git-install.sh
+++ install/git-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-GIT_VERSION="1.7.11.4"
+GIT_VERSION="1.7.12.4"
 INSTALL_BASE="/usr/local" # will make base/git-v.v.v.v folder and link base/git to it
 SOURCE_FOLDER="$HOME/sources"
 
