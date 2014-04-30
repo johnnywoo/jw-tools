b781c0e81efa70851781d09955f55c01f27ee9c9
Aleksandr Galkin <agalkin@agalkin.ru>
2014-04-30 21:40:03 +0400

    Removed some old crud

--- personal/install.sh
+++ personal/install.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# changes CWD! luckily we don't care
-realpath()
-{
+realpath() {
 	cd "$1"
 	pwd
 }
 
-fld=$(realpath "$(dirname "$0")")
+dir=$(realpath "$(dirname "$0")")
 
 echo -ne "Installing inputrc... "
 if [ -e "$HOME/.inputrc" ]
 then
 	echo "already exists"
 else
-	ln -s "$fld/inputrc" "$HOME/.inputrc"
+	ln -s "$dir/inputrc" "$HOME/.inputrc"
 	echo "done"
 fi
 
@@ -24,7 +22,7 @@ if [ -e "$HOME/.gitconfig" ]
 then
 	echo "already exists"
 else
-	ln -s "$fld/gitconfig" "$HOME/.gitconfig"
+	ln -s "$dir/gitconfig" "$HOME/.gitconfig"
 	echo "done"
 fi
 
@@ -33,14 +31,14 @@ echo -ne "Installing profile... "
 profile_fname=$(ls -1 "$HOME/.bash_profile" "$HOME/.bash_login" "$HOME/.profile" 2>/dev/null | head -n 1)
 [ -z "$profile_fname" ] && profile_fname="$HOME/.profile" && touch "$profile_fname"
 
-if grep "$fld" "$profile_fname" &>/dev/null
-then
+if grep "$dir" "$profile_fname" &>/dev/null; then
 	echo "already installed"
 else
-	echo '' >> "$profile_fname"
-	echo 'PS1_COLOR=white # red green blue yellow cyan pink gray white' >> "$profile_fname"
+	echo >> "$profile_fname"
+	echo 'PS1_COLOR=gray # red green blue yellow cyan pink gray white' >> "$profile_fname"
 	echo '# we need this variable, please do not remove it' >> "$profile_fname"
-	echo "JW_PERSONAL_DIR='$fld'" >> "$profile_fname"
+	echo "JW_PERSONAL_DIR='$dir'" >> "$profile_fname"
 	echo 'source "$JW_PERSONAL_DIR/bash-profile"' >> "$profile_fname"
+
 	echo "done (you can change PS1_COLOR in ~/$(basename $profile_fname))"
 fi
